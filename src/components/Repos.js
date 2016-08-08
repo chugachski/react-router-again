6d7be6469ae2c3e1147a9910892be43d5722f0db
Syed Salahuddin <syedamin@cs.nyu.edu>
2016-08-08 12:30:58 -0400

    updates to class

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavLink from './NavLink';
+import { browserHistory } from 'react-router';
 
-function Repos(props){
-  return(
-    <div>
-      <h2>Repos</h2>
-      <ul>
-        <li><NavLink to="/repos/sssyed/wdi-robots"> WDI Robots</NavLink></li>
-        <li><NavLink to="/repos/sssyed/arduino"> Arduino </NavLink></li>
-      </ul>
-      { props.children }
-    </div>
-  );
+class Repos extends Component{
+
+  handleSubmit(event){
+    event.preventDefault();
+    const userName = event.target.elements[0].value;
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`;
+    //console.log(path);
+    browserHistory.push(path);
+  }
+
+  render(){
+    return(
+      <div>
+        <h2>Repos</h2>
+        <ul>
+          <li><NavLink to="/repos/sssyed/wdi-robots"> WDI Robots</NavLink></li>
+          <li><NavLink to="/repos/sssyed/arduino"> Arduino </NavLink></li>
+          <li>
+            <form onSubmit={(event) => this.handleSubmit(event)}>
+              <input type="text" placeholder="userName"/>{' '}
+              <input type="text" placeholder="repo"/>{' '}
+              <button type="submit">Go</button>
+            </form>
+          </li>
+        </ul>
+        { this.props.children }
+      </div>
+    );
+  }
 }
 
 export default Repos;
